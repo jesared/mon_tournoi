3ddc799599176d9a39027c6bc10d91d8daa47460
jerome jesared <jesared@gmail.com>
2025-01-24 14:07:20 +0100

    Update deployment 24/01/2025

---
+++ src/app/dashboard/tournois/page.tsx
@@ -0,0 +1,8 @@
+export default function TournoisPage() {
+  return (
+    <div>
+      {" "}
+      <h1 className="text-2xl font-bold mb-4">Tournois</h1>
+    </div>
+  );
+}
--- src/app/tournois/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Tournois() {
-  return <div>Tournois</div>;
-}
